917cc1758e333de6edb39f791aa85ac0895647ff
bV <bv@bvector.net>
2014-04-06 22:33:07 +0000

    add arbitrary coin lookup (.ltc for example)

--- cryptocoin.py
+++ cryptocoin.py
@@ -7,6 +7,12 @@ def ticker(bot, trigger):
     bot.say('BTC: {}  LTC: {}  DOGE: {}'.format(prices['btc'], prices['ltc'], prices['doge']))
 
 
+@willie.module.rule(r'\.([a-z42]{2,4})')
+def cryptocoin(bot, trigger):
+    prices = update_prices()
+    if trigger.group(1) in prices:
+        bot.say(trigger.group(1) + ':' +prices[trigger.group(1)])
+
 def update_prices():
     prices = {}
     r = requests.get('http://altcoinfolio.com/cryptofolio/public/js/coins.json').json()
